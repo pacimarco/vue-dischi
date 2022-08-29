01ae783f65d70a3b4b08c244820edc8cd2285d83
pacimarco <m.paci1987@gmail.com>
2022-08-29 16:02:05 +0200

    fixare errore img height diverse

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -2,12 +2,12 @@
   <div class="col">
       <div class="card text-center bg-secondary">
       <div class="img-album img-fluid">
-          <img class="album.poster img-fluid card-img-top" :src="album.poster" alt="">
+          <img class="album-poster img-fluid img-thumbnail card-img-top" :src="album.poster" alt="">
       </div>
       <div class="card-body">
-          <h5 class="text-white">{{album.title}}</h5>
-      <h6 class="text-dark">{{album.author}}</h6>
-      <h6 class="text-dark">{{album.year}}</h6>
+          <h5 class="text-white card-title">{{album.title}}</h5>
+      <h6 class="text-dark card-text">{{album.author}}</h6>
+      <h6 class="text-dark card-text">{{album.year}}</h6>
       </div>
   </div>
   </div>
